88e544eb8481c8bdf6154783b84dc16802acd2aa
Sarah Lopez <slopez323@gmail.com>
2022-03-09 15:21:41 -0500

    completed

---
+++ main.js
@@ -0,0 +1,24 @@
+let p = document.querySelector('p');
+p.style.color = 'lightblue';
+
+let h1 = document.querySelector('h1');
+h1.style.fontSize = '48px';
+
+p.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
+
+let item13 = document.querySelector('#item-13');
+item13.style.opacity = "50%";
+
+let item3 = document.querySelector('#item-3');
+item3.innerText = `I say, "Hi!"`
+
+let img = document.querySelector('img');
+img.src = 'http://www.tioxic.com/wp-content/uploads/trex_4.jpg';
+img.style.height = '300px';
+
+let newimg = document.querySelector('#new-img');
+newimg.style.height = '300px';
+
+let item16 = document.querySelector('#item-16');
+item16.className = 'item';
+item16.innerText = `Won't get fooled again.`;
